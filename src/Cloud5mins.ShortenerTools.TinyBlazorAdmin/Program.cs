11106870bc49da36e72ddf86c720bef965c11935
David Oliva Paredes <david_oliva_paredes@hotmail.com>
2024-04-18 14:18:58 +0200

    Update local.settings.example.json, UrlManager.razor, Program.cs, and NavMenu.razor

--- src/Cloud5mins.ShortenerTools.TinyBlazorAdmin/Program.cs
+++ src/Cloud5mins.ShortenerTools.TinyBlazorAdmin/Program.cs
@@ -3,13 +3,14 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Syncfusion.Blazor;
 using Cloud5mins.ShortenerTools.TinyBlazorAdmin;
 using AzureStaticWebApps.Blazor.Authentication;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 var baseAddress = builder.HostEnvironment.BaseAddress;
 builder.Services
-        .AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) })
+        .AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:7071/") })
         .AddStaticWebAppsAuthentication();
 
 // builder.Services.AddMsalAuthentication(options =>
@@ -19,7 +20,7 @@ builder.Services
 
 // regiser fusion blazor service
 // Community Licence for your personal use ONLY. Thank you Syncfusion for this generous offer.
-Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NzYyMzI1QDMyMzAyZTMxMmUzMFY0cEZ3MVozdkwvekVhek8xTWdPMkg2NlhvdVFNR1lvZHdhQWJWUlNjZW89"); 
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NzYyMzI1QDMyMzAyZTMxMmUzMFY0cEZ3MVozdkwvekVhek8xTWdPMkg2NlhvdVFNR1lvZHdhQWJWUlNjZW89");
 builder.Services.AddSyncfusionBlazor();
 
 await builder.Build().RunAsync();
